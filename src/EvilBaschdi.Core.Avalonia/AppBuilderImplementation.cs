1b4539138e422496f1164a513687610dddf167a6
Sebastian Walter <sebwalter@gmx.net>
2024-02-23 17:04:41 +0100

    removed arm specific RenderingMode

--- src/EvilBaschdi.Core.Avalonia/AppBuilderImplementation.cs
+++ src/EvilBaschdi.Core.Avalonia/AppBuilderImplementation.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.ReactiveUI;
 
 namespace EvilBaschdi.Core.Avalonia;
@@ -14,13 +13,13 @@ public class AppBuilderImplementation<TApp> : IAppBuilderImplementation
         get
         {
             var win32PlatformOptions = new Win32PlatformOptions();
-            if (RuntimeInformation.OSArchitecture == Architecture.Arm || RuntimeInformation.OSArchitecture == Architecture.Arm64)
-            {
-                win32PlatformOptions.RenderingMode = new List<Win32RenderingMode>
-                                                     {
-                                                         Win32RenderingMode.Wgl
-                                                     };
-            }
+            //if (RuntimeInformation.OSArchitecture == Architecture.Arm || RuntimeInformation.OSArchitecture == Architecture.Arm64)
+            //{
+            //    win32PlatformOptions.RenderingMode = new List<Win32RenderingMode>
+            //                                         {
+            //                                             Win32RenderingMode.Wgl
+            //                                         };
+            //}
 
             return AppBuilder.Configure<TApp>()
                              .UsePlatformDetect()
