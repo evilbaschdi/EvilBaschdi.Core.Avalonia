f25d4d9534f615b4177008d8bd4cbc7f7d2635b6
Sebastian Walter <sebwalter@gmx.net>
2024-02-24 12:06:09 +0100

    removed MessageBox.Avalonia in favour of FluentAvaloniaUI

---
+++ src/EvilBaschdi.Core.Avalonia/Controls/ContentDialogInput.axaml.cs
@@ -0,0 +1,46 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Threading;
+
+namespace EvilBaschdi.Core.Avalonia.Controls;
+
+/// <inheritdoc />
+public partial class ContentDialogInput : UserControl
+{
+    /// <summary>
+    ///     Text to display as caption inside the dialog
+    /// </summary>
+    public string CaptionText { get; init; }
+
+    /// <summary>
+    ///     Text to transport the result text
+    /// </summary>
+    public string ResultText { get; private set; }
+
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    public ContentDialogInput()
+    {
+        InitializeComponent();
+        Caption.Text = CaptionText;
+    }
+
+    // ReSharper disable once UnusedParameter.Local
+    private void InputFieldOnAttachedToVisualTree(object sender, VisualTreeAttachmentEventArgs e)
+    {
+        // We will set the focus into our input field just after it got attached to the visual tree.
+        if (sender is InputElement inputElement)
+        {
+            Dispatcher.UIThread.InvokeAsync(() => { inputElement.Focus(); });
+        }
+    }
+
+    // ReSharper disable UnusedParameter.Local
+    private void ResultOnTextChanged(object sender, TextChangedEventArgs e)
+        // ReSharper restore UnusedParameter.Local
+    {
+        ResultText = Result.Text;
+    }
+}
