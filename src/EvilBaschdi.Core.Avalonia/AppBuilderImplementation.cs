cb5180b640192b66be4b16666369d08f1b735b71
Sebastian Walter <sebwalter@gmx.net>
2024-10-04 08:01:18 +0200

    updated Avalonia to v11.2.0-rc1

--- src/EvilBaschdi.Core.Avalonia/AppBuilderImplementation.cs
+++ src/EvilBaschdi.Core.Avalonia/AppBuilderImplementation.cs
@@ -13,13 +13,6 @@ public class AppBuilderImplementation<TApp> : IAppBuilderImplementation
         get
         {
             var win32PlatformOptions = new Win32PlatformOptions();
-            //if (RuntimeInformation.OSArchitecture == Architecture.Arm || RuntimeInformation.OSArchitecture == Architecture.Arm64)
-            //{
-            //    win32PlatformOptions.RenderingMode = new List<Win32RenderingMode>
-            //                                         {
-            //                                             Win32RenderingMode.Wgl
-            //                                         };
-            //}
 
             return AppBuilder.Configure<TApp>()
                              .UsePlatformDetect()
